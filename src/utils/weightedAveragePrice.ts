c5efc30d09156a29a2af1fbd7a5929b35b301ee7
Ömer Burak KARATAŞ <omer.burak.karatas@outlook.com>
2020-07-25 17:32:13 +0300

    feat: add util functions

---
+++ src/utils/weightedAveragePrice.ts
@@ -0,0 +1,16 @@
+import { IMarket } from '../models/interfaces/IMarket';
+
+export const weightedAveragePrice = (markets: IMarket[]) => {
+  let marketsHavingTicker = markets.filter(({ ticker }) => !!ticker);
+
+  const totalBaseVolume = marketsHavingTicker.reduce((acc, { ticker }) => {
+    let tickerBaseVolume = parseFloat(ticker?.baseVolume as string) || 0;
+    return acc + tickerBaseVolume;
+  }, 0);
+
+  return marketsHavingTicker.reduce((acc, { ticker }) => {
+    const averagePrice = ticker ? (parseFloat(ticker.lowPrice) + parseFloat(ticker.lowPrice)) / 2 : 0;
+
+    return acc + averagePrice * (parseFloat(ticker?.baseVolume as string) / totalBaseVolume) * 100;
+  }, 0);
+};
