3446b3a110417e4a0d14294337a535594e68dd4e
Ömer Burak KARATAŞ <omer.burak.karatas@outlook.com>
2020-07-25 20:25:26 +0300

    feat: add MarketCard component

--- src/models/interfaces/IMarket.ts
+++ src/models/interfaces/IMarket.ts
@@ -1,7 +1,9 @@
 import { ITicker } from './ITicker';
 
 export interface IMarket {
+  id?: string;
   marketType?: string;
+  exchangeSymbol?: string;
   marketSymbol?: string;
   baseSymbol?: string;
   quoteSymbol?: string;
