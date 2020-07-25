148f0c9c4d3996ad83654abffd4a33ae1860767d
Ömer Burak KARATAŞ <omer.burak.karatas@outlook.com>
2020-07-25 21:11:52 +0300

    feat: add mobile support

--- src/components/asset-summary/AssetSummary.tsx
+++ src/components/asset-summary/AssetSummary.tsx
@@ -14,11 +14,21 @@ const AssetSummary: React.FC<IAssetSummaryProps> = ({ asset: { id, name, symbol,
   return (
     <div className="asset-summary-container" key={id} onClick={() => history.push(`/markets/${name}`)}>
       <div className="name" title={name}>
+        <div className="label">Name:</div>
         {name}
       </div>
-      <div className="pair">{`${symbol}/USD`}</div>
-      <div className="market-cap">{currencyFormatter(marketCap)}</div>
-      <div className="average-last-price">{currencyFormatter(weightedAveragePrice(markets))}</div>
+      <div className="pair">
+        <div className="label">Pair:</div>
+        {`${symbol}/USD`}
+      </div>
+      <div className="market-cap">
+        <div className="label">Market Cap:</div>
+        {currencyFormatter(marketCap)}
+      </div>
+      <div className="average-last-price">
+        <div className="label">Average Last Price:</div>
+        {currencyFormatter(weightedAveragePrice(markets))}
+      </div>
     </div>
   );
 };
