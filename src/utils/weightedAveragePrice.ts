a259ebd7cc4471cad45322fc6594e8cbf7be9ee9
Ömer Burak KARATAŞ <omer.burak.karatas@outlook.com>
2020-07-25 21:32:17 +0300

    chore: add explanation for weighted average price calculation

--- src/utils/weightedAveragePrice.ts
+++ src/utils/weightedAveragePrice.ts
@@ -1,5 +1,17 @@
 import { IMarket } from '../models/interfaces/IMarket';
 
+/**
+ *
+ * @description How is it computed?
+ * First filter markets having no analytics information, i.e ticker. Then first
+ * calculate total base volume, which is base asset volume traded in past 24 hours(
+ * according to API documentation).
+ * By using total base volume, for each market find corresponding weighted proportions
+ * in all markets. For each market I assume that the average of all trades is the average
+ * of low price and high price in last 24 hours. By multiplying the average amount of
+ * each market with its weights, summing them, the weighted average price is calculated.
+ */
+
 export const weightedAveragePrice = (markets: IMarket[]) => {
   let marketsHavingTicker = markets.filter(({ ticker }) => !!ticker);
 
